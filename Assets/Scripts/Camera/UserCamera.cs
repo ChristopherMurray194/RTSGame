f5f449dc5562cc27c17ba804477a2fa68a9b61f3
Christopher Murray <chris.murray194@hotmail.co.uk>
2017-03-18 23:08:11 +0000

    Camera is rotated and translated using a parent transform.

--- Assets/Scripts/Camera/UserCamera.cs
+++ Assets/Scripts/Camera/UserCamera.cs
@@ -7,11 +7,11 @@ public class UserCamera : MonoBehaviour
     /// <summary> Camera movement speed </summary>
     public float movementSpeed = 2.0f;
     /// <summary> Camera rotation speed </summary>
-    public float rotationSpeed = 1.0f;
+    public float rotationSpeed = 4.0f;
 
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -24,37 +24,46 @@ public class UserCamera : MonoBehaviour
         // Get inputs
         // The value will be in the range -1...1. For keyboard input this will either be -1, 0, 1
         float h = Input.GetAxisRaw("Horizontal");   // Maps to A and D keys
-        float v = 0;                                // Mapped to X and C keys
+        float v = 0f;                               // Mapped to X and C keys
         if (Input.GetKey(KeyCode.C))
-            v = 1;
+            v = 1f;
         else if (Input.GetKey(KeyCode.X))
-            v = -1;
-        else v = 0;
+            v = -1f;
+        else v = 0f;
         float z = Input.GetAxisRaw("Vertical");     // Maps to W and S keys
-
+        // Apply movement
         Move(h, v, z);
-        Rotate(h, v, z);
+
+        float r = 0f;                               // Maps to Q and E keys
+        if (Input.GetKey(KeyCode.E))
+            r = 1f;
+        else if (Input.GetKey(KeyCode.Q))
+            r = -1f;
+        else r = 0f;
+        // Apply yaw rotation
+        Yaw(r);
     }
 
     /// <summary>
-    /// Handles camera movement based on the given inputs; h and v.
+    /// Handles the camera parent transform (not the camera's iteself) movement based on the given inputs; h and v.
     /// </summary>
-    /// <param name="h"> The horizontal axis input </param>
-    /// <param name="v"> The vertical axis input </param>
+    /// <param name="h"> Horizontal direction defined by user input; Left if -1, Right if 1 </param>
+    /// <param name="v"> Vertical direction defined by user input; Up if 1, Down if -1 </param>
+    /// <param name="z"> Forward direction defined by user input; Forward if 1, Backwards if -1 </param>
     void Move(float h, float v, float z)
     {
-        Transform transform = GetComponent<Transform>();
-        // Obtain the new camera position
-        Vector3 position = transform.position;
-        position.x += h * movementSpeed * Time.deltaTime;
-        position.y += v * movementSpeed * Time.deltaTime;
-        position.z += z * movementSpeed * Time.deltaTime;
-        // Update the camera's position
-        transform.position = position;
+        transform.Translate((h * movementSpeed * Time.deltaTime),
+                            (v * movementSpeed * Time.deltaTime),
+                            (z * movementSpeed * Time.deltaTime),
+                            Space.Self); // Move in the forward direction of the camera parent (NOT the camera)
     }
 
-    void Rotate(float h, float v, float z)
+    /// <summary>
+    /// Handles the camera parent yaw rotation (not the camera's iteself) based on the value of r.
+    /// </summary>
+    /// <param name="r"> Rotation direction defined by user input; Clockwise if 1, Counter-clockwise if -1 </param>
+    void Yaw(float r)
     {
-
+        transform.Rotate(0f, (r * rotationSpeed), 0f, Space.World);
     }
 }
