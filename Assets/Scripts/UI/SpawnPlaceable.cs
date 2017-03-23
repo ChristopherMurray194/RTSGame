dc7c14b4b7a906d37b4e4bcf2a01c2cc37b29687
Christopher Murray <chris.murray194@hotmail.co.uk>
2017-03-23 23:05:33 +0000

    UI canvas created and button added to spawn pizza shop placeable.

---
+++ Assets/Scripts/UI/SpawnPlaceable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlaceable : MonoBehaviour
+{
+    /// <summary> The placeable object to spawn </summary>
+    public GameObject placeable;
+
+	void Start ()
+    {
+	    	
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Create an instance of the placeable object.
+    /// </summary>
+    public void SpawnObject()
+    {
+        Instantiate(placeable);
+    }
+
+}
