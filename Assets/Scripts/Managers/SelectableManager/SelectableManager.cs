71299497f86cb6d9886b5dad41c77108609120e4
Christopher Murray <chris.murray194@hotmail.co.uk>
2017-11-27 17:46:39 +0000

    A Marker is shown above the currently selected object.

--- Assets/Scripts/Managers/SelectableManager/SelectableManager.cs
+++ Assets/Scripts/Managers/SelectableManager/SelectableManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class SelectableManager : MonoBehaviour
 {
     int selectableMask;
-    Selectable selectedObject;
+    GameObject selectedObject;
+
+    /// <summary> The game object that is displayed to the player to show which object has been selected. </summary>
+    public GameObject selectionMarker;
+    public float markerOffset = 1f;
 
 
     void Start()
@@ -19,6 +24,11 @@ public class SelectableManager : MonoBehaviour
         {
             GetSelectedObject();
         }
+
+        if (selectedObject != null)
+            PositionMarker();
+        else
+            selectionMarker.SetActive(false);
     }
 
     /// <summary>
@@ -29,16 +39,15 @@ public class SelectableManager : MonoBehaviour
         // Cast a ray from the main camera to the mouse position
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        GameObject hitObject;
 
         // If the ray hits an object on the selectable layer
         if (Physics.Raycast(camRay, out hit, 1000f, selectableMask))
         {
             // Get the object that was hit
-            hitObject = hit.collider.gameObject;
+            selectedObject = hit.collider.gameObject;
             // Notify the object it has been selected
-            selectedObject = hitObject.GetComponent<Selectable>();
-            selectedObject.IsSelected = true;
+            Selectable selectableScript = selectedObject.GetComponent<Selectable>();
+            selectableScript.IsSelected = true;
         }
         // Otherwise we are not clicking on a selectable object
         else if (selectedObject != null)
@@ -46,6 +55,22 @@ public class SelectableManager : MonoBehaviour
             // Notify the currently selected object it has been deselected.
             //selectedObject.IsSelected = false;
             selectedObject = null;
+        }
+    }
+
+    /// <summary>
+    /// Position the selectionMarker above the selected object.
+    /// </summary>
+    void PositionMarker()
+    {
+        selectionMarker.SetActive(true);
+
+        Collider col = selectedObject.GetComponent<Collider>();
+        if(col != null)
+        {
+            Vector3 markerPos = selectedObject.transform.position;
+            markerPos.y += col.bounds.extents.magnitude + markerOffset;
+            selectionMarker.transform.position = markerPos;
         }
     }
 }
